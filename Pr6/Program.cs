745f14bed56ea64bfd8c29498369875a2613853e
Sovenyatko <alexandryshka@ukr.net>
2023-05-31 02:04:56 +0300

    Prak6

---
+++ Pr6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] lines = File.ReadAllLines("input.txt");
+        sortedLines = lines.OrderBy(line => line).ToArray
+        string[] ();
+
+        File.WriteAllLines("output.txt", sortedLines);
+    }
+}
+
